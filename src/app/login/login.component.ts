51aa196d509046b1b403240851e91fd5b704f83d
marioe <marioe@thehangar.cr>
2017-07-29 16:55:42 -0600

    Login added WIP

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,OnInit,Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  login(user: HTMLInputElement, pass: HTMLInputElement): boolean {
+
+    let valid = user.value =='mario' && pass.value =='pass' ? alert('pase') : alert('error');
+
+    return false;
   }
 
 }
